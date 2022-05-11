cf9365f8297f50ec7b3cbd9b39cf63d07f5b9eb1
Garth <garthcline@gmail.com>
2022-05-11 16:02:24 -0400

    Upgrade

--- idle/idle.js
+++ idle/idle.js
@@ -10,17 +10,45 @@ const upgradeCost1 = document.getElementById('cost-upgrade1')
 const upgradeCost2 = document.getElementById('cost-upgrade2')
 const autoCost1 = document.getElementById('cost-auto1')
 const autoCost2 = document.getElementById('cost-auto2')
+const stuffMakerCost = document.getElementById('cost-stuff-maker')
 const clickButton1 = document.getElementById('btn-click1')
 const clickButton2 = document.getElementById('btn-click2')
 const tick1Text = document.getElementById('tick1')
 const tick2Text = document.getElementById('tick2')
 const auto1Text = document.getElementById('auto1')
 const auto2Text = document.getElementById('auto2')
+const thingMakerEnable = document.getElementById('thing-maker-enable')
+const stuffMakerEnable = document.getElementById('stuff-maker-enable')
+const thingMakerButton = document.getElementById('btn-thing-maker')
+const stuffMakerButton = document.getElementById('btn-stuff-maker')
+const row1 = document.getElementById('row1')
+const row2 = document.getElementById('row2')
+const shopButton = document.getElementById('shop')
+const boostText = document.getElementById('boost-text')
 
 let tickValue1 = 1
 let tickValue2 = 10
 let auto1Count = 0
 let auto2Count = 0
+let boost = 1
+
+shopButton.addEventListener('click', async () => {
+    boost++
+    boostText.classList.remove('invisible')
+    boostText.childNodes[1].childNodes[1].innerText = boost
+})
+
+thingMakerButton.addEventListener('click', async () => {
+    row1.classList.remove('invisible')
+    thingMakerEnable.classList.add('invisible')
+    stuffMakerEnable.classList.add('visible')
+
+})
+
+stuffMakerButton.addEventListener('click', async () => {
+    row2.classList.remove('invisible')
+    stuffMakerEnable.classList.add('invisible')
+})
 
 clickButton1.addEventListener('click', async () => {
     tick1()
@@ -33,7 +61,7 @@ clickButton2.addEventListener('click', async () => {
 autoButton1.addEventListener('click', async () => {
     const oldTotal = Number(total.innerText)
     total.innerText = oldTotal - Number(autoCost1.innerText)
-    autoCost1.innerText = Number(autoCost1.innerText) + Number(autoCost1.innerText) / 2
+    autoCost1.innerText = Number(autoCost1.innerText) * 2
     checkCosts()
     setInterval(tick1, 1000)
     auto1Count++
@@ -43,9 +71,9 @@ autoButton1.addEventListener('click', async () => {
 autoButton2.addEventListener('click', async () => {
     const oldTotal = Number(total.innerText)
     total.innerText = oldTotal - Number(autoCost2.innerText)
-    autoCost2.innerText = Number(autoCost2.innerText) + Number(autoCost2.innerText) / 2
+    autoCost2.innerText = Number(autoCost2.innerText) * 2
     checkCosts()
-    setInterval(tick2, 5000)
+    setInterval(tick2, 2500)
     auto2Count++
     auto2Text.innerText = auto2Count
 })
@@ -55,7 +83,7 @@ upgradeButton1.addEventListener('click', async () => {
     total.innerText = oldTotal - Number(upgradeCost1.innerText)
     tickValue1++
     const oldCost = Number(upgradeCost1.innerText)
-    upgradeCost1.innerText = oldCost * 2
+    upgradeCost1.innerText = oldCost + 10
     checkCosts()
     tick1Text.innerText = tickValue1
 })
@@ -65,7 +93,7 @@ upgradeButton2.addEventListener('click', async () => {
     total.innerText = oldTotal - Number(upgradeCost2.innerText)
     tickValue2 += 5
     const oldCost = Number(upgradeCost2.innerText)
-    upgradeCost2.innerText = oldCost * 2
+    upgradeCost2.innerText = oldCost + 25
     checkCosts()
     tick2Text.innerText = tickValue2
 
@@ -73,7 +101,7 @@ upgradeButton2.addEventListener('click', async () => {
 
 function tick1() {
     const oldValue = Number(total.innerText)
-    const newValue = oldValue + tickValue1
+    const newValue = oldValue + tickValue1 * boost
     total.innerText = newValue
 
     checkCosts()
@@ -81,7 +109,7 @@ function tick1() {
 
 function tick2() {
     const oldValue = Number(total.innerText)
-    const newValue = oldValue + tickValue2
+    const newValue = oldValue + tickValue2 * boost
     total.innerText = newValue
 
     checkCosts()
@@ -110,5 +138,11 @@ function checkCosts() {
         autoButton2.classList.remove('disabled')
     } else {
         autoButton2.classList.add('disabled')
+    }
+
+    if (Number(total.innerHTML) >= Number(stuffMakerCost.innerText)) {
+        stuffMakerButton.classList.remove('disabled')
+    } else {
+        stuffMakerButton.classList.add('disabled')
     }
 }
