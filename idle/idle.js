f4c76c5771aac5464ddfa4bc96438910ec4fa134
Garth <garthcline@gmail.com>
2022-05-11 00:07:26 -0400

    beta

--- idle/idle.js
+++ idle/idle.js
@@ -1,18 +1,38 @@
-console.log('Renderer Start')
+console.log('App Start')
 //let $ = require('jquery')
 
-const btn = document.getElementById('btn')
-const counter = document.getElementById('counter')
-let tick = 1
+const total = document.getElementById('total')
+const upgradeButton = document.getElementById('btn-upgrade')
+const upgradeCost = document.getElementById('cost')
+const clickButton = document.getElementById('btn-click')
 
-function func() {
-    const oldValue = Number(counter.innerText)
-    const newValue = oldValue + tick
-    counter.innerText = newValue
-}
-btn.addEventListener('click', async () => {
-    const oldValue = Number(counter.innerText)
-    const newValue = oldValue + tick
-    counter.innerText = newValue
-    setInterval(func, 1000)
+let tickValue = 1
+
+clickButton.addEventListener('click', async () => {
+    tick()
 })
+
+upgradeButton.addEventListener('click', async () => {
+    const oldTotal = Number(total.innerText)
+    total.innerText = oldTotal - Number(upgradeCost.innerText)
+    tickValue++
+    const oldCost = Number(upgradeCost.innerText)
+    upgradeCost.innerText = oldCost * 2
+    checkCosts()
+})
+
+function tick() {
+    const oldValue = Number(total.innerText)
+    const newValue = oldValue + tickValue
+    total.innerText = newValue
+
+    checkCosts()
+}
+
+function checkCosts() {
+    if (Number(total.innerHTML) >= Number(upgradeCost.innerText)) {
+        upgradeButton.classList.remove('disabled')
+    } else {
+        upgradeButton.classList.add('disabled')
+    }
+}
